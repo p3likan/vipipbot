0236b641fbd14d5cfb2a3f4ec67bc7d31b8104af
p3likan <cnapob@gmail.com>
2017-01-22 22:45:36 +0300

    #3

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 var settings=require('./js/settings.js');
 var http=require('http');
 var static=require('node-static');
-var fileServer=new static.Server('.');
+var fileServer=new static.Server('./settingsserver');
 var port=5534;
 var s=new settings('./test.txt');
 http.createServer(function(request,response)
 {
 	if(request.url==='/')
 	{
-		
+		fileServer.serve(request,response);
 	}
 	else if(request.url==='/query/')
 	{
@@ -19,4 +19,4 @@ http.createServer(function(request,response)
 }).listen(port);
 console.log('Open http://127.0.0.1:'+port+'/ to change settings and see progress.');
 
-console.log('>',s.getAccountsList(console.log.bind(console,'<')));
+//console.log('>',s.getAccountsList(console.log.bind(console,'<')));
