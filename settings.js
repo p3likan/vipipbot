43fce2921915b256d91488a87f37dfd24dd7631e
p3likan <cnapob@gmail.com>
2017-01-22 14:19:08 +0300

    download node-static

--- settings.js
+++ settings.js
@@ -1,3 +1,22 @@
 var settings=require('./js/settings.js');
+var http=require('http');
+var static=require('node-static');
+var fileServer=new static.Server('.');
+var port=5534;
 var s=new settings('./test.txt');
+http.createServer(function(request,response)
+{
+	if(request.url==='/')
+	{
+		
+	}
+	else if(request.url==='/query/')
+	{
+		
+	}
+	else
+		fileServer.serve(request,response);
+}).listen(port);
+console.log('Open http://127.0.0.1:'+port+'/ to change settings and see progress.');
+
 console.log('>',s.getAccountsList(console.log.bind(console,'<')));
